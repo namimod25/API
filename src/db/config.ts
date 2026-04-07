d0464852d5a75e98f0dc6ac67f746f42a4a1e1eb
nami <namimod25@gmail.com>
2026-04-07 19:10:49 +0700

    configdb

--- src/db/config.ts
+++ src/db/config.ts
@@ -4,7 +4,7 @@ import { PrismaPg } from '@prisma/adapter-pg';
 import pg from 'pg';
 
 const prismaClientSingleton = () => {
-  const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
+  const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL!});
   const adapter = new PrismaPg(pool);
   return new PrismaClient({
     adapter,
