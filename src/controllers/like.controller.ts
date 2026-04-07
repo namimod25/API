8dda90c30c4baeb5ca0ee15ca41135e771f4ab04
nami <namimod25@gmail.com>
2026-04-07 18:40:46 +0700

    like-controller

--- src/controllers/like.controller.ts
+++ src/controllers/like.controller.ts
@@ -8,7 +8,7 @@ export const LikeFedd = async (req: Request, res: Response) => {
     const currentUserId = req.data?.id
     const {postId} = req.params
 
-    if (!currentUserId) {
+    if (currentUserId === undefined) {
         return res.status(401).json({ message: "Unauthorized" })
     }
 
@@ -25,7 +25,7 @@ export const LikeFedd = async (req: Request, res: Response) => {
         const CheckLike = await prisma.likes.findUnique({
             where: {
                 userId_postId: {
-                    userId: currentUserId!,
+                    userId: currentUserId,
                     postId: Number(postId)
                 },
             }
@@ -39,7 +39,7 @@ export const LikeFedd = async (req: Request, res: Response) => {
 
         const newLike = await prisma.likes.create({
             data:{
-                userId: currentUserId!,
+                userId: currentUserId,
                 postId: Number(postId)
             }
         });
@@ -58,9 +58,70 @@ export const LikeFedd = async (req: Request, res: Response) => {
 
     } catch (error) {
         console.log(error);
-        res.status(500).json({
-            message: "internal server",
+        
+        res.status(500).json({message: "internal server",
             error
         });
+    }
+}
+
+
+export const CheckUserLike = async (req: Request, res: Response) => {
+    const {postId} = req.params
+    const currentUserId = req.data?.id
+
+    if (currentUserId === undefined) {
+        return res.status(401).json({ message: "Unauthorized" })
+    }
+
+    try {
+        const postData = await prisma.post.findUnique({
+            where:{
+                id: Number(postId)
+            }
+        })
+
+        if(!postData){
+            return  res.status(404).json({
+                message: "anda belum like post/feed tersebut"
+            })
+        }
+
+        const CheckLike = await prisma.likes.findUnique({
+            where:{
+                userId_postId: {
+                    userId: currentUserId,
+                    postId: Number(postId)
+                }
+            }
+        });
+
+        if(CheckLike){
+            await prisma.likes.delete({
+                where:{
+                    userId_postId: {
+                        userId: currentUserId,
+                        postId: Number(postId)
+                    }
+                }
+            })
+
+            await prisma.post.update({
+                where: {id: Number(postId)},
+                data: {
+                    likeCount: {decrement: 1}
+                }
+            })
+            return res.status(200).json({message: "Unlike Berhasil"});
+
+        }
+        return res.status(200).json({
+            data: false
+        })
+
+    } catch (error) {
+         return res.status(500).json({
+                message:"internal server", error
+            })
     }
 }
